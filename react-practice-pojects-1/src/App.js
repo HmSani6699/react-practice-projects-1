242095b3c277d1bba144c9f78e836e9fb4be03f0
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-24 14:37:06 +0600

    complete author blog part

--- react-practice-pojects-1/src/App.js
+++ react-practice-pojects-1/src/App.js
@@ -14,9 +14,12 @@ function App() {
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium nisi facilis voluptas rem asperiores, nobis molestiae. Delectus, aut veritatis doloremque ad consequatur expedita quas dignissimos error, eligendi, id et commodi explicabo deleniti? Illo repudiandae et ut maxime omnis voluptatibus quod, ab animi vitae tempore quo in reprehenderit quis, obcaecati odio?</p>
         <p style={{ color: 'white', backgroundColor: 'orange', padding: '10px' }}>inline style</p>
       </article>
-      <Blog></Blog>
-      <Blog></Blog>
-      <Blog></Blog>
+      <Blog heading="Insa allah ekdin sob asa poron hobe"
+        author="tomra sobay amar jono doawa koro ami jeno amar sob asa poron korte pari"></Blog>
+      <Blog heading="Insa allah ekdin sob asa poron hobe"
+        author="tomra sobay amar jono doawa koro ami jeno amar sob asa poron korte pari"></Blog>
+      <Blog heading="Insa allah ekdin sob asa poron hobe"
+        author="tomra sobay amar jono doawa koro ami jeno amar sob asa poron korte pari"></Blog>
     </div>
   );
 }
--- react-practice-pojects-1/src/component/Blog/Blog.js
+++ react-practice-pojects-1/src/component/Blog/Blog.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Blog.css'
 
-const Blog = () => {
+const Blog = (props) => {
     return (
         <div className='author-blog'>
-            <h1>Alhamdolillah vallo</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor quae blanditiis ipsum quidem saepe, non in commodi, voluptas dolorem repellendus a pariatur officia. Odio iusto vero unde asperiores ratione. Esse.</p>
+            <h1>{props.heading}</h1>
+            <p>{props.author}</p>
         </div>
     );
 };
