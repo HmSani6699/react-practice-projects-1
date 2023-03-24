e230c9311c0d0285be43fa08bd89d3f864f05659
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-24 14:22:14 +0600

    practice task two and three complete

--- react-practice-pojects-1/src/App.js
+++ react-practice-pojects-1/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 
 function App() {
+  const titleStyle = {
+    color: 'blue',
+    backgroundColor: 'gray',
+    padding: '10px'
+  }
   return (
     <div className="App">
-      <h1>New blog</h1>
       <article className='blog'>
+        <h1 style={titleStyle}>New blog</h1>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium nisi facilis voluptas rem asperiores, nobis molestiae. Delectus, aut veritatis doloremque ad consequatur expedita quas dignissimos error, eligendi, id et commodi explicabo deleniti? Illo repudiandae et ut maxime omnis voluptatibus quod, ab animi vitae tempore quo in reprehenderit quis, obcaecati odio?</p>
+        <p style={{ color: 'white', backgroundColor: 'orange', padding: '10px' }}>inline style</p>
       </article>
     </div>
   );
