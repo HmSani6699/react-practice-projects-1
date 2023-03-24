e1cbc70f564a2a8c56ed38d266ed11cf9e97cd1f
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-24 14:14:03 +0600

    ad new projects

---
+++ react-practice-pojects-1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <article>
+        <h1>New blog</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id debitis, earum ut placeat est ratione ducimus autem eligendi veniam esse sunt voluptas laudantium illum tempore modi maxime repudiandae, enim nemo.</p>
+      </article>
+    </div>
+  );
+}
+
+export default App;
